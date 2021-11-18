84b5d14ffbd122824e46c2d54ae899d5070cebd7
Pedro Gomes <pedrogomes2101@gmail.com>
2021-11-18 18:11:48 -0300

    some_every

--- fundamentos/bloco08-Higher_Order_Functions/02-forEach_find_some_every_sort/some_every.js
+++ fundamentos/bloco08-Higher_Order_Functions/02-forEach_find_some_every_sort/some_every.js
@@ -3,3 +3,17 @@ const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
 const hasName = (arr, name) => arr.some((contido) => contido === name);
 
 console.log(hasName(names, 'Ana'))
+// --------------------------------------------------------------------
+const people = [
+    { name: 'Mateus', age: 18 },
+    { name: 'José', age: 16 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => 
+    arr.every((name) => name.age >= minimumAge)
+
+
+console.log(verifyAges(people, 18));
