341d693f3c7edf361c72611a80ca9c01d06950d3
Pedro Gomes <pedrogomes2101@gmail.com>
2021-11-19 18:16:52 -0300

    exercicio1 done

--- fundamentos/bloco08-Higher_Order_Functions/03-map-filter/exercise1.js
+++ fundamentos/bloco08-Higher_Order_Functions/03-map-filter/exercise1.js
@@ -61,4 +61,18 @@ const books = [
     },
 ];
 
-  // Adicione o código do exercício aqui:
+const expectedResult = [
+    'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+    'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+    'Fundação - Ficção Científica - Isaac Asimov',
+    'Duna - Ficção Científica - Frank Herbert',
+    'A Coisa - Terror - Stephen King',
+    'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+];
+
+function formatedBookNames() {
+    const livros = books.map((categoria) => `${categoria.name} - ${categoria.genre} - ${categoria.author.name}`);
+    return livros;
+}
+
+console.log(formatedBookNames());
