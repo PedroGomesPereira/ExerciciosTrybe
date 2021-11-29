15bfaf7253ba0b1019100e7c4ce34199021d667b
Pedro Gomes <pedrogomes2101@gmail.com>
2021-11-29 17:10:35 -0300

    fixar2

--- fundamentos/bloco09-javaScript-testes_assincronos/01-assincrono-callbacks/callbacks.js
+++ fundamentos/bloco09-javaScript-testes_assincronos/01-assincrono-callbacks/callbacks.js
@@ -12,3 +12,25 @@ const getUser = (callback) => {
 
 console.log(getUser(userFullName)); // complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
 console.log(getUser(userNationality)); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian"
+
+// ----------
+
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const getUser = (callback) => {
+    setTimeout(() => {
+        const user = {
+            firstName: "Ivan",
+            lastName: "Ivanovich",
+            nationality: "Russian",
+        };
+        let c = callback(user);
+        console.log(c);
+    }, delay());
+};
+
+getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
+getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
