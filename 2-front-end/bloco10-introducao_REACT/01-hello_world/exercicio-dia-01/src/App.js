5c6a761c48b5c5e3aaf0d6d227b4ff0d847659d4
Pedro Gomes <pedrogomes2101@gmail.com>
2021-12-09 19:16:23 -0300

    compromisos

--- 2-front-end/bloco10-introducao_REACT/01-hello_world/exercicio-dia-01/src/App.js
+++ 2-front-end/bloco10-introducao_REACT/01-hello_world/exercicio-dia-01/src/App.js
@@ -1,25 +1,17 @@
+import react from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const compromisos = ['estudar', 'exercitar', 'jogar com amigos', 'descansar']
+
+const Task = (value) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <li>{value}</li>
   );
+}
+
+function App() {
+  return compromisos.map((item) => Task(item));
 }
 
 export default App;
